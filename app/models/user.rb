197fed507326f111b3ce9bbcbac93177368949b6
Illarion Kopeika <larrypearson@yandex.ru>
2026-08-17 16:21:05 +0800

    navbar, errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   normalizes :email, with: ->(e) { e.strip.downcase }
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { message: :taken_email }
   validates :password, presence: true
-  validates :name, presence: true
+  validates :name, presence: { message: :blank_name }
 end
